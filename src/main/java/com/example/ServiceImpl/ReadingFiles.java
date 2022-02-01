a7cd7eb5f11d46cb660c25a0f360742998a4be52
Aashish Reddy <37476786+Aashishreddy@users.noreply.github.com>
2022-02-01 16:47:01 -0600

    Reading, Writing to Files
    Using Stream API

--- src/main/java/com/example/ServiceImpl/ReadingFiles.java
+++ src/main/java/com/example/ServiceImpl/ReadingFiles.java
@@ -9,10 +9,16 @@ public class ReadingFiles {
     public static void main(String args[]) throws IOException {
         String filePath = "C:\\Users\\Axv200051\\IdeaProjects\\Demo\\src\\main\\java\\com\\example\\ServiceImpl\\Read.txt";
 
+        // READING FROM A FILE
         try(Stream<String> stream = Files.lines(Paths.get(filePath))){
-            stream.forEach( x -> {
-                System.out.println(x);
+//            stream.forEach(System.out::println);
+            stream.forEach(line -> {
+                System.out.println(line);
             });
         }
+
+        //WRITING TO A FILE
+        String input = "You got it";
+        Files.write(Paths.get(filePath), input.getBytes());
     }
 }
